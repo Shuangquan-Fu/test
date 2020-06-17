7930b39e565c8323eeb3ea021cf814746e9af222
shufu <fsq19981210@gmail.com>
2020-06-17 16:09:45 -0400

    fixing User Entity

---
+++ src/main/java/com/shuangquan/ppmtool/services/UserService.java
@@ -0,0 +1,11 @@
+package com.shuangquan.ppmtool.services;
+
+import com.shuangquan.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
